cceca12923f725ba92c496313c2205a87c9b1068
TheAnsumanAJ <163329573+TheAnsumanAJ@users.noreply.github.com>
2026-01-11 10:50:23 +0530

    Refactor app.js for improved structure and updates

--- app.js
+++ app.js
@@ -1,30 +1,35 @@
-import express from 'express';
-import userRouter from './routes/user.js';
-import taskRouter from './routes/task.js';
-import { config } from 'dotenv';
-import cookieParser from 'cookie-parser';
+import express from "express";
+import userRouter from "./routes/user.js";
+import taskRouter from "./routes/task.js";
+import { config } from "dotenv";
+import cookieParser from "cookie-parser";
+import { errorMiddleware } from "./middlewares/error.js";
 import cors from "cors";
 
 export const app = express();
 
 config({
-    path: "./features/config.env",
+  path: "./data/config.env",
 });
 
-app.use(cors({
-    origin: process.env.FRONTEND_URL,
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
-}));
-
-app.options("*", cors()); // ✅ handle preflight
-
+// Using Middlewares
 app.use(express.json());
 app.use(cookieParser());
+app.use(
+  cors({
+    origin: [process.env.FRONTEND_URL],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 
-app.use(userRouter);
-app.use("/tasks", taskRouter);
+// Using routes
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/task", taskRouter);
 
 app.get("/", (req, res) => {
-    res.send("Hello AJ!");
+  res.send("Nice working");
 });
+
+// Using Error Middleware
+app.use(errorMiddleware);
